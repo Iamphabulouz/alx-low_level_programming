3ec0d0afc8e09ac00429c092252b846a19c86271
Iamphabulouz <cynthia.anamekwe@gmail.com>
2023-02-21 16:19:25 -0800

    eight commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
@@ -11,7 +12,12 @@ void jack_bauer(void)
     {
         for (minute = 0; minute < 60; minute++)
         {
-            printf("%02d:%02d\n", hour, minute);
+            putchar((hour / 10) + '0');
+            putchar((hour % 10) + '0');
+            putchar(':');
+            putchar((minute / 10) + '0');
+            putchar((minute % 10) + '0');
+            putchar('\n');
         }
     }
 }
