ff5f6cf1f7e0cbd87d9bbdfa1c9cc0815a94a0b3
Iamphabulouz <cynthia.anamekwe@gmail.com>
2023-02-22 02:53:12 -0800

    first commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,17 @@
+carrie@ubuntu:/debugging$ cat main.c
 #include "main.h"
 
 /**
- * main - tests the positive_or_negative function
- * that prints if integer is positive or negative
- *
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-    int i;
+        int i;
 
-    i = 0;
-    positive_or_negative(i);
+        i = 0;
+        positive_or_negative(i);
 
-    return (0);
+        return (0);
 }
