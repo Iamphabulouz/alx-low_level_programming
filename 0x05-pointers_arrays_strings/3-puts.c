069d39be50d274e6210315a6e1223cea0c92d6b0
Iamphabulouz <cynthia.anamekwe@gmail.com>
2023-02-27 06:53:37 -0800

    fourth commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * _puts - Prints a string followed by a new line to stdout
  * @str: Pointer to the string to be printed
@@ -9,7 +9,7 @@ void _puts(char *str)
 
     for (i = 0; str[i] != '\0'; i++)
     {
-        _putchar(str[i]);
+      _putchar(str[i]);
     }
     _putchar('\n');
 }
