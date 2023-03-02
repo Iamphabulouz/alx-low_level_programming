f3f27d7a9b4d9ab9d28587a18083a3b7e170554b
Iamphabulouz <cynthia.anamekwe@gmail.com>
2023-03-02 02:07:31 -0800

    main.h

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,24 @@
  * Return: returns poiner to @dest
  */
 
-char *_strcat(char *dest, char *src)
+char* _strcat(char* dest, char* src)
 {
+    char* ptr = dest;
 
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+    // Move the pointer to the end of the dest string
+    while (*ptr != '\0') {
+        ptr++;
+    }
+    
+    // Append the source string to the end of the dest string
+    while (*src != '\0') {
+        *ptr = *src;
+        ptr++;
+        src++;
+    }
+    
+    // Add a null-terminating character to the end of the string
+    *ptr = '\0';
+    
+    return dest;
 }
