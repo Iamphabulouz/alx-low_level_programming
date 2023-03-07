c163c8f5e7660955aa7ea61532e83ead1c987fa2
Iamphabulouz <cynthia.anamekwe@gmail.com>
2023-03-07 12:59:11 -0800

    first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to string to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+    if (*s == '\0') /* base case */
+    {
+        _putchar('\n'); /* print newline character and return */
+        return;
+    }
+
+    _putchar(*s); /* print current character */
+    _puts_recursion(s + 1); /* call function recursively with next character */
+}
